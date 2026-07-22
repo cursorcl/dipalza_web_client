64377e09120b17a57b0bf4f91268d9d3c993bea3
Eliecer Osorio V. <cursor.cl@gmail.com>
2026-07-21 22:57:01 -0400

    fix: add missing HttpClient test provider in mapa.component.spec.ts
    TestBed was missing provideHttpClient()/provideHttpClientTesting(),
    which PositionsService needs. Pre-existing gap in the project's test
    infra, unrelated to this feature, but Tasks 2 and 4 depend on this
    spec compiling and passing.

--- src/app/mapa/mapa.component.spec.ts
+++ src/app/mapa/mapa.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { MapaComponent } from './mapa.component';
 
@@ -8,7 +10,8 @@ describe('MapaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MapaComponent]
+      imports: [MapaComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
     .compileComponents();
 
